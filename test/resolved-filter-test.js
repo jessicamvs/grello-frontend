a6db809072cde517428556ff5274e314a09cd61d
Jessica Soltero <jmichellevs@gmail.com>
2017-04-17 10:21:25 -0700

    pass this time

--- test/resolved-filter-test.js
+++ test/resolved-filter-test.js
@@ -15,16 +15,16 @@ describe('Resolved Filter', function() {
     expect(angular.isArray(this.resolvedFilter(this.tasks))).toBe(true)
   })
 
-  // it('should leave tasks array unmodified', () => {
-  //   this.resolvedFilter(this.tasks)
-  //   expect(this.tasks).toBe(this.tasks)
-  // })
-  //
-  // it('should return non-resovled tasks', () => {
-  //   const filteredTasks = this.resolvedFilter(this.tasks)
-  //   expect(filteredTasks.length).toBe(2)
-  //   filteredTasks.forEach(task => {
-  //     expect(task.resolved).toBe(false)
-  //   })
-  // })
+  it('should leave tasks array unmodified', () => {
+    this.resolvedFilter(this.tasks)
+    expect(this.tasks).toBe(this.tasks)
+  })
+
+  it('should return non-resovled tasks', () => {
+    const filteredTasks = this.resolvedFilter(this.tasks)
+    expect(filteredTasks.length).toBe(2)
+    filteredTasks.forEach(task => {
+      expect(task.resolved).toBe(false)
+    })
+  })
 })
